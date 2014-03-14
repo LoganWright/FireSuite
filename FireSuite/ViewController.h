95abaaf441c7ae26213b4136b63981e826d8e35b
Logan Wright <logan.william.wright@gmail.com>
2014-03-14 02:03:17 -0400

    Initial Commit

---
+++ FireSuite/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FireSuite
+//
+//  Created by Logan Wright on 3/14/14.
+//  Copyright (c) 2014 Logan Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
